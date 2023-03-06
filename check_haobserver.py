51f1eb6ddf6f7466b48ab0c2e32fae1517df4112
Toby Creek <48847748+halfordfan@users.noreply.github.com>
2023-03-06 16:05:28 -0500

    Create check_haobserver.py
    Initial commit

---
+++ check_haobserver.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Written by Toby Creek, distributed under the MIT License
+# Basic operability check of HomeAssistant via the haobserver on port 4357
+# This was written to help in monitoring HAOS as basic access to the operating system
+# is frought with peril.
+
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+if len(sys.argv) == 1:
+  print('USAGE: ' + sys.argv[0] + ' hostname [local]')
+  exit(0)
+
+if len(sys.argv) > 2 and sys.argv[2] == 'local':
+  print('<<<local>>>')
+else:
+  print('<<<check_mk>>>')
+  print('Version: 1.0')
+  print('AgentOS: HomeAssistant')
+
+try:
+  response=requests.get('http://' + sys.argv[1] + ':4357/')
+  soup=BeautifulSoup(response.text.replace('\r','').replace('\n','').replace('\t',''), "html.parser")
+  table = soup.find('table')
+  statrows = table.find_all('tr')
+  statuses={}
+except:
+  exit(0)
+
+for row in statrows:
+  #print(row)
+  service=row.find('td', {'class': []}).contents[0].string.strip().replace(':','')
+  try:
+    status=row.find('td', {'class': ['connected','disconnected']})
+    if status.attrs['class'][0] == 'connected':
+      # OK
+      code='0'
+      perf=' connected=1 '
+    if status.attrs['class'][0] == 'disconnected':
+      # CRIT
+      code='2'
+      perf=' connected=0 '
+    nicestatus=status.contents[0].string.strip()
+  except:
+    # Unknown
+    code='3'
+    nicestatus="Unknown"
+    perf=' - '
+  print(code + ' ' + service + perf + service + ' reports ' + nicestatus)
